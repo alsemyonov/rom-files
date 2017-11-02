9e83021b2dae29ebd2c74552d797b3bb04e6d9d2
Alex Semyonov <alex@semyonov.us>
2017-11-03 04:41:54 +0500

    Fix Types loading

--- lib/rom/files/types.rb
+++ lib/rom/files/types.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 require 'dry/types/pathname'
+require 'rom/types'
 
 module ROM
   module Files
     module Types
-      include Dry::Types.module
+      include ROM::Types
     end
   end
 end
