0b605863dbe0efa714e6dc91ab07b9fcd417b205
Alex Semyonov <alex@semyonov.us>
2017-11-03 05:50:17 +0500

    Spec for Gateway#use_logger

--- spec/unit/rom/files/gateway_spec.rb
+++ spec/unit/rom/files/gateway_spec.rb
@@ -7,8 +7,16 @@ require 'shared/filesystem_setup'
 RSpec.describe ROM::Files::Gateway do
   include_context 'filesystem setup'
 
-  let(:gateway) { ROM::Files::Gateway }
+  subject(:gateway_instance) { ROM::Gateway.setup(identifier, uri) }
+  let(:gateway) { described_class }
   let(:identifier) { :files }
 
   it_behaves_like "a rom gateway"
+
+  describe '#use_logger' do
+    it 'sets logger' do
+      gateway_instance.use_logger(:logger)
+      expect(gateway_instance.logger).to eq :logger
+    end
+  end
 end
