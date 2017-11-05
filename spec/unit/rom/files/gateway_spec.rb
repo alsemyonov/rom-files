b12029d11a8e94a6aa93a324de23ba851b32267a
Alex Semyonov <alex@semyonov.us>
2017-11-05 18:39:49 +0500

    Add spec for Gateway#dataset?

--- spec/unit/rom/files/gateway_spec.rb
+++ spec/unit/rom/files/gateway_spec.rb
@@ -2,10 +2,10 @@
 
 require 'rom/lint/spec'
 require 'rom/files/gateway'
-require 'shared/filesystem_setup'
+require 'shared/media_relation'
 
 RSpec.describe ROM::Files::Gateway do
-  include_context 'filesystem setup'
+  include_context 'media relation'
 
   subject(:gateway_instance) { ROM::Gateway.setup(identifier, uri) }
   let(:gateway) { described_class }
@@ -17,6 +17,22 @@ RSpec.describe ROM::Files::Gateway do
     it 'sets logger' do
       gateway_instance.use_logger(:logger)
       expect(gateway_instance.logger).to eq :logger
+    end
+  end
+
+  describe '#dataset?' do
+    subject { gateway_instance.dataset?(relation) }
+
+    context 'with existing relation' do
+      let(:relation) { :media }
+
+      it { is_expected.to be true }
+    end
+
+    context 'with non-existing relation' do
+      let(:relation) { :not_here }
+
+      it { is_expected.to be false }
     end
   end
 end
