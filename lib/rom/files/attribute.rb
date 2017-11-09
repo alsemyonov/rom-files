9bc6ffef7bbbfb9b37ac96a6617be2ac6ab2e3ef
Alex Semyonov <alex@semyonov.us>
2017-11-09 07:00:45 +0500

    Make attributes callable

--- lib/rom/files/attribute.rb
+++ lib/rom/files/attribute.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
+require 'pathname'
 require 'rom/attribute'
 
 module ROM
   module Files
     class Attribute < ROM::Attribute
+      # @param [Pathname] pathname
+      # @return [Object]
+      def call(pathname) # rubocop:disable Metrics/AbcSize
+        return type[pathname.read] if meta[:__contents__]
+        return type[pathname.stat] if meta[:__stat__].is_a?(TrueClass)
+        return type[pathname.stat.send(meta[:__stat__])] if meta[:__stat__]
+        type[pathname]
+      end
     end
   end
 end
--- lib/rom/files/schema.rb
+++ lib/rom/files/schema.rb
@@ -10,6 +10,28 @@ module ROM
     #
     # @api public
     class Schema < ROM::Schema
+      # Abstract method for creating a new relation based on schema definition
+      #
+      # This can be used by views to generate a new relation automatically.
+      # In example a schema can project a relation, join any additional relations
+      # if it uncludes attributes from other relations etc.
+      #
+      # Default implementation is a no-op and it simply returns back untouched relation
+      #
+      # @param [Relation] relation
+      #
+      # @return [Relation]
+      #
+      # @api public
+      def call(relation)
+        relation.class.new(relation.dataset.with(row_proc: method(:load_attributes)))
+      end
+
+      def load_attributes(row_proc)
+        each do |attribute|
+          row_proc[attribute.name] = attribute.call(row_proc[:__FILE__])
+        end
+      end
     end
   end
 end
