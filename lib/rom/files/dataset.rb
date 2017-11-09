b1db93944f2430b0c33493ea372719556ddd5b62
Alex Semyonov <alex@semyonov.us>
2017-11-09 06:34:55 +0500

    Remove filtering method from Dataset
    They are included in Filtering mixin

--- lib/rom/files/dataset.rb
+++ lib/rom/files/dataset.rb
@@ -68,47 +68,6 @@ module ROM
         end
       end
 
-      # @!group Reading
-
-      # @return [Dataset]
-      def select(*patterns)
-        with(includes: patterns.uniq)
-      end
-
-      # @return [Dataset]
-      def select_append(*patterns)
-        with(includes: (includes + patterns).uniq)
-      end
-
-      # @return [Dataset]
-      def reject(*patterns)
-        with(excludes: patterns.uniq)
-      end
-
-      # @return [Dataset]
-      def reject_append(*patterns)
-        with(excludes: (excludes + patterns).uniq)
-      end
-
-      # @return [Dataset]
-      def sort(sort_by = :to_s)
-        with(sort_by: sort_by)
-      end
-
-      # @return [Dataset]
-      def inside(*prefixes)
-        select(*prefixes.inject([]) do |result, prefix|
-          result + includes.map do |pattern|
-            "#{prefix}/#{pattern}"
-          end
-        end)
-      end
-
-      # @return [Dataset]
-      def recursive
-        inside '**'
-      end
-
       # @return [Boolean]
       def recursive?
         includes.all? { |pattern| pattern =~ RECURSIVE_EXPRESSION }
