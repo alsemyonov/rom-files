75fbac1ec8f9f9d517f3a090b95879f790024448
Alex Semyonov <alex@semyonov.us>
2017-11-09 05:09:41 +0500

    (dry-types) Do not over-register pathname types

--- lib/dry/types/pathname.rb
+++ lib/dry/types/pathname.rb
@@ -6,11 +6,15 @@ require 'pathname'
 
 module Dry
   module Types
-    register 'pathname', simple = Definition[Pathname].new(Pathname)
-    register 'strict.pathname', strict = simple.constrained(type: Pathname)
-    register 'coercible.pathname',
-             coercible = simple.constructor(Kernel.method(:Pathname))
-    register 'optional.strict.pathname', strict.optional
-    register 'optional.coercible.pathname', coercible.optional
+    def self.register_if_needed(key, value)
+      register(key, value) unless registered?(key)
+    end
+
+    register_if_needed 'pathname', simple = Definition[Pathname].new(Pathname)
+    register_if_needed 'strict.pathname', strict = simple.constrained(type: Pathname)
+    register_if_needed 'coercible.pathname',
+                       coercible = simple.constructor(Kernel.method(:Pathname))
+    register_if_needed 'optional.strict.pathname', strict.optional
+    register_if_needed 'optional.coercible.pathname', coercible.optional
   end
 end
