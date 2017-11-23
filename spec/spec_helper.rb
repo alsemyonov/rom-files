7a2a2c5799c4e17ae8a7f9f25311616010e667c5
Alex Semyonov <alex@semyonov.us>
2017-11-23 20:54:29 +0300

    Simplify spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,14 +8,3 @@ require 'simplecov'
 require 'rom-files'
 
 SPEC_ROOT = Pathname(__dir__)
-
-RSpec.configure do |config|
-  config.before do
-    @constants = Object.constants
-  end
-
-  config.after do
-    added_constants = Object.constants - @constants
-    added_constants.each { |name| Object.send(:remove_const, name) }
-  end
-end
