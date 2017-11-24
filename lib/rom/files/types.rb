9fa87cb18118b5cfc257e322af10689f83e59127
Alex Semyonov <alex@semyonov.us>
2017-11-24 03:17:42 +0300

    Provide Types.ForeignKey

--- lib/rom/files/types.rb
+++ lib/rom/files/types.rb
@@ -17,6 +17,21 @@ module ROM
       MimeType = Dry::Types::Definition[MIME::Type].new(MIME::Type).optional.constructor do |type|
         MIME::Types[type].first
       end
+
+      # Define a foreign key attribute type
+      #
+      # @example with default type
+      #   attribute :spec_file, Types.ForeignKey(:spec_files)
+      #
+      # @example with a custom path map
+      #   attribute :spec_file, Types.ForeignKey(:spec_files, key: ->(path) { path.pathmap('spec/%X_spec.rb') })
+      #
+      # @return [Dry::Types::Definition]
+      #
+      # @api public
+      def self.ForeignKey(relation, type = Types::Pathname, map: ->(pathname) { pathname })
+        super(relation, type.meta(__proc__: map))
+      end
     end
   end
 end
