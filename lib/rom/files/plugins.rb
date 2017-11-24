7cbd05c77e4b4536bf897344d504cf14fbdc7c48
Alex Semyonov <alex@semyonov.us>
2017-11-24 18:54:46 +0300

    Add `:shebang` schema plugin

--- lib/rom/files/plugins.rb
+++ lib/rom/files/plugins.rb
@@ -4,12 +4,14 @@ require 'rom/plugins/relation/files/instrumentation'
 
 require 'rom/files/plugins/schema/contents'
 require 'rom/files/plugins/schema/mime'
+require 'rom/files/plugins/schema/shebang'
 require 'rom/files/plugins/schema/stat'
 
 ROM.plugins do
   adapter :files do
     register :contents, ROM::Files::Plugins::Schema::Contents, type: :schema
     register :mime, ROM::Files::Plugins::Schema::Mime, type: :schema
+    register :shebang, ROM::Files::Plugins::Schema::Shebang, type: :schema
     register :stat, ROM::Files::Plugins::Schema::Stat, type: :schema
   end
 end
--- lib/rom/files/plugins/schema/shebang.rb
+++ lib/rom/files/plugins/schema/shebang.rb
@@ -7,58 +7,65 @@ module ROM
   module Files
     module Plugins
       module Schema
-        # A plugin for automatically adding contents of file
+        # A plugin for automatically adding shebang of file
         # to the schema definition
         #
         # @example Generic `DATA` field with String type
         #   schema do
-        #     use :contents
+        #     use :shebang
         #   end
         #
         # @example Specify another type
         #   schema do
-        #     use :contents, type: Types::YAML
+        #     use :shebang, type: Types::YAML
         #   end
         #
         # @example Specify another name
         #   # using other types
         #   schema do
-        #     use :contents, name: :contents
+        #     use :shebang, name: :shebang
         #   end
         #
         # @api public
-        module Contents
-          NAME = Files::DATA
+        module Shebang
           TYPE = Types::String
+          NAME = :run_with
+
+          # @param path [Pathname]
+          # @return [String, nil]
+          def self.read_shebang(path)
+            shebang = path.readlines.first || ''
+            shebang[2..-1].chomp if shebang.match?(/\A#!/)
+          end
 
           # @api private
           def self.apply(schema, name: NAME, type: TYPE)
-            contents = type.meta(name: name, source: schema.name, DATA: true)
+            shebang = type.meta(name: name, source: schema.name, __proc__: method(:read_shebang))
 
             schema.attributes.concat(
-              schema.class.attributes([contents], schema.attr_class)
+              schema.class.attributes([shebang], schema.attr_class)
             )
           end
 
           # @api private
           module DSL
-            # Sets non-default contents attribute
+            # Sets non-default shebang attribute
             #
             # @example Set custom attribute name
             #   schema do
-            #     use :contents
-            #     contents :contents
+            #     use :shebang
+            #     shebang :shebang
             #   end
             #
             # @example Set custom type
             #   schema do
-            #     use :contents
-            #     contents type: Types::JSON
+            #     use :shebang
+            #     shebang type: Types::JSON
             #   end
             #
             # @api public
-            def contents(name = NAME, inline_type = TYPE, type: inline_type)
-              options = plugin_options(:contents)
+            def shebang(name = NAME, inline = TYPE, type: inline)
+              options = plugin_options(:shebang)
               options[:name] = name
               options[:type] = type
 
