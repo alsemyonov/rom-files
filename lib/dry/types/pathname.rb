40a6e5b462aa0c411a93bd188eb32985958d2d6a
Alex Semyonov <alex@semyonov.us>
2017-11-28 03:28:21 +0300

    Extract Dry::Types.Pathname method for coercion

--- lib/dry/types/pathname.rb
+++ lib/dry/types/pathname.rb
@@ -10,10 +10,14 @@ module Dry
       register(key, value) unless registered?(key)
     end
 
+    def self.Pathname(path)
+      path.is_a?(Pathname) ? path : Kernel::Pathname(path.to_s)
+    end
+
     register_if_needed 'pathname', simple = Definition[Pathname].new(Pathname)
     register_if_needed 'strict.pathname', strict = simple.constrained(type: Pathname)
     register_if_needed 'coercible.pathname',
-                       coercible = simple.constructor(->(path) { Pathname(path.to_s) })
+                       coercible = simple.constructor(method(:Pathname))
     register_if_needed 'optional.pathname', simple.optional
     register_if_needed 'optional.strict.pathname', strict.optional
     register_if_needed 'optional.coercible.pathname', coercible.optional
