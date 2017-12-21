258ede32670c666eed179065743b203cf372fa12
Alex Semyonov <alex@semyonov.us>
2017-12-21 05:08:13 +0300

    Provide shortcuts for Attribute

--- lib/rom/files/attribute.rb
+++ lib/rom/files/attribute.rb
@@ -10,17 +10,56 @@ module ROM
       # @param [Pathname] pathname
       # @return [Object]
       def call(pathname, root: nil)
-        return type[pathname.relative_path_from(root)] if meta[:relative]
-        return type[pathname.read] if meta[Files::DATA]
-        return type[pathname.stat] if meta[:__stat__].is_a?(TrueClass)
-        return type[pathname.stat.send(meta[:__stat__])] if meta[:__stat__]
-
-        if meta[:__proc__]
-          proc = meta[:__proc__].respond_to?(:call) ? meta[:__proc__] : meta[:__proc__].to_proc
-          return type[proc.(pathname)]
-        end
+        return type[pathname.relative_path_from(root)] if relative?
+        return type[pathname.read] if data?
+        return type[pathname.stat] if stat?
+        return type[pathname.stat.send(stat_property)] if stat_property?
+        return type[processor.(pathname)] if processor?
         type[pathname]
       end
+
+      # @return [Boolean]
+      def stat?
+        meta[:__stat__].is_a?(TrueClass)
+      end
+
+      # @return [Symbol]
+      def stat_property
+        return nil if stat?
+        meta[:__stat__]
+      end
+
+      # @return [Boolean]
+      def stat_property?
+        !stat? && meta[:__stat__]
+      end
+
+      # @return [Boolean]
+      def content?
+        meta[:content].is_a?(TrueClass)
+      end
+
+      # @return [Boolean]
+      def data?
+        meta[Files::DATA].is_a?(TrueClass)
+      end
+
+      # @return [Boolean]
+      def processor?
+        meta[:__proc__]
+      end
+
+      # @return [#call]
+      def processor
+        meta[:__proc__].respond_to?(:call) ? meta[:__proc__] : meta[:__proc__].to_proc
+      end
+
+      # @return [Boolean]
+      def relative?
+        meta[:relative]
+      end
+
+      memoize :stat?, :stat_property?, :content?, :data?, :processor?, :processor, :relative?
     end
   end
 end
--- spec/lib/rom/files/attribute_spec.rb
+++ spec/lib/rom/files/attribute_spec.rb
@@ -4,10 +4,8 @@ require 'rom/files/attribute'
 require 'shared/rom/files/media_relation'
 
 RSpec.describe ROM::Files::Attribute do
-  Types = ROM::Files::Types
-
   subject(:attribute) { described_class.new(type.meta(name: name)) }
-  let(:type) { Types::String }
+  let(:type) { ROM::Files::Types::String }
   let(:name) { :attr_name }
   let(:path) { Pathname(__FILE__) }
 
@@ -17,33 +15,73 @@ RSpec.describe ROM::Files::Attribute do
     subject(:call) { attribute.method(:call) }
 
     context 'with relative:' do
-      let(:type) { Types::RelativePath }
+      let(:type) { ROM::Files::Types::RelativePath }
 
       its([Pathname(__FILE__), root: Pathname(__dir__)]) { is_expected.to eq Pathname('attribute_spec.rb') }
     end
 
     context 'with DATA' do
-      let(:type) { Types::String.meta(DATA: true) }
+      let(:type) { ROM::Files::Types::String.meta(DATA: true) }
 
       its([Pathname(__FILE__)]) { is_expected.to eq File.read(__FILE__) }
     end
+  end
 
-    context 'with stat' do
-      let(:type) { Types::FileStat.meta(__stat__: true) }
+  describe '#processor?' do
+    context '(__proc__: Symbol)' do
+      let(:type) { ROM::Files::Types::String.meta(__proc__: :extname) }
 
-      its([Pathname(__FILE__)]) { is_expected.to eq File.stat(__FILE__) }
+      its(:processor?) { is_expected.to be_truthy }
+      its(:processor) { is_expected.to be_a Proc }
+
+      specify { expect(attribute.(Pathname(__FILE__))).to eq '.rb' }
     end
 
-    context 'with specific stat' do
-      let(:type) { Types::Int.meta(__stat__: :mode) }
+    context '(__proc__: Proc)' do
+      let(:type) { ROM::Files::Types::String.meta(__proc__: ->(path) { path.extname }) }
 
-      its([Pathname(__FILE__)]) { is_expected.to eq 0o0100644 }
+      its(:processor?) { is_expected.to be_truthy }
+      its(:processor) { is_expected.to be_a Proc }
+
+      specify { expect(attribute.(Pathname(__FILE__))).to eq '.rb' }
+    end
+  end
+
+  describe '#data?' do
+    let(:type) { ROM::Files::Types::String.meta(DATA: true) }
+
+    its(:data?) { is_expected.to be_truthy }
+
+    specify { expect(attribute.(Pathname(__FILE__))).to eq File.read(__FILE__) }
+  end
+
+  describe '#stat?, #stat_property, #stat_property?' do
+    context 'meta()' do
+      let(:type) { ROM::Files::Types::FileStat }
+
+      its(:stat?) { is_expected.to be_falsey }
+      its(:stat_property) { is_expected.to eq nil }
+      its(:stat_property?) { is_expected.to be_falsey }
     end
 
-    context 'with proc' do
-      let(:type) { Types::String.meta(__proc__: ->(path) { path.extname }) }
+    context 'meta(__stat__: true)' do
+      let(:type) { ROM::Files::Types::FileStat.meta(__stat__: true) }
 
-      its([Pathname(__FILE__)]) { is_expected.to eq '.rb' }
+      its(:stat?) { is_expected.to eq true }
+      its(:stat_property) { is_expected.to eq nil }
+      its(:stat_property?) { is_expected.to eq false }
+
+      specify { expect(attribute.(Pathname(__FILE__))).to eq File.stat(__FILE__) }
+    end
+
+    context 'meta(__stat__: :atime)' do
+      let(:type) { ROM::Files::Types::FileStat.meta(__stat__: :mode) }
+
+      its(:stat?) { is_expected.to eq false }
+      its(:stat_property) { is_expected.to eq :mode }
+      its(:stat_property?) { is_expected.to be_truthy }
+
+      specify { expect(attribute.(Pathname(__FILE__))).to eq 0o0100644 }
     end
   end
 end
